d4c8b088f83d1388e35099c1daf4d02a429c82e0
Fernando Rodrigues <98523060+Fe7rodrigues@users.noreply.github.com>
2022-11-13 19:02:22 -0300

    iniciando o dark light theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,9 +41,35 @@ window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+   // Quando a rolagem for maior que 350 de altura da janela de visualização, adicione a classe show-scroll à tag a com a classe scrollup
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
